11ded0fc668be5dd6111f9d289c9b9b1c4167763
Brian Smith <brian@linuxfood.net>
2012-01-20 10:39:08 -0800

    Update to use commas instead of semi-colons in enums.

--- sqlite.rs
+++ sqlite.rs
@@ -40,57 +40,57 @@ import result::{ok, err, success};
 // export sqlite_open;
 
 enum sqlite_result_code {
-  SQLITE_OK         =  0;
-  SQLITE_ERROR      =  1;
-  SQLITE_INTERNAL   =  2;
-  SQLITE_PERM       =  3;
-  SQLITE_ABORT      =  4;
-  SQLITE_BUSY       =  5;
-  SQLITE_LOCKED     =  6;
-  SQLITE_NOMEM      =  7;
-  SQLITE_READONLY   =  8;
-  SQLITE_INTERRUPT  =  9;
-  SQLITE_IOERR      = 10;
-  SQLITE_CORRUPT    = 11;
-  SQLITE_NOTFOUND   = 12;
-  SQLITE_FULL       = 13;
-  SQLITE_CANTOPEN   = 14;
-  SQLITE_PROTOCOL   = 15;
-  SQLITE_EMPTY      = 16;
-  SQLITE_SCHEMA     = 17;
-  SQLITE_TOOBIG     = 18;
-  SQLITE_CONSTRAINT = 19;
-  SQLITE_MISMATCH   = 20;
-  SQLITE_MISUSE     = 21;
-  SQLITE_NOLFS      = 22;
-  SQLITE_AUTH       = 23;
-  SQLITE_FORMAT     = 24;
-  SQLITE_RANGE      = 25;
-  SQLITE_NOTADB     = 26;
-  SQLITE_ROW        = 100;
-  SQLITE_DONE       = 101;
+  SQLITE_OK         =  0,
+  SQLITE_ERROR      =  1,
+  SQLITE_INTERNAL   =  2,
+  SQLITE_PERM       =  3,
+  SQLITE_ABORT      =  4,
+  SQLITE_BUSY       =  5,
+  SQLITE_LOCKED     =  6,
+  SQLITE_NOMEM      =  7,
+  SQLITE_READONLY   =  8,
+  SQLITE_INTERRUPT  =  9,
+  SQLITE_IOERR      = 10,
+  SQLITE_CORRUPT    = 11,
+  SQLITE_NOTFOUND   = 12,
+  SQLITE_FULL       = 13,
+  SQLITE_CANTOPEN   = 14,
+  SQLITE_PROTOCOL   = 15,
+  SQLITE_EMPTY      = 16,
+  SQLITE_SCHEMA     = 17,
+  SQLITE_TOOBIG     = 18,
+  SQLITE_CONSTRAINT = 19,
+  SQLITE_MISMATCH   = 20,
+  SQLITE_MISUSE     = 21,
+  SQLITE_NOLFS      = 22,
+  SQLITE_AUTH       = 23,
+  SQLITE_FORMAT     = 24,
+  SQLITE_RANGE      = 25,
+  SQLITE_NOTADB     = 26,
+  SQLITE_ROW        = 100,
+  SQLITE_DONE       = 101,
 }
 
 enum sqlite_bind_arg {
-  text(str);
-  number(float);
-  integer(int);
-  blob([u8]);
-  null();
+  text(str),
+  number(float),
+  integer(int),
+  blob([u8]),
+  null(),
 }
 
 enum sqlite_column_type {
-  sqlite_integer;
-  sqlite_float;
-  sqlite_text;
-  sqlite_blob;
-  sqlite_null;
+  sqlite_integer,
+  sqlite_float,
+  sqlite_text,
+  sqlite_blob,
+  sqlite_null,
 }
 
 type sqlite_result<T> = result::t<T, sqlite_result_code>;
 enum sqlite_row_result {
-  row(map::hashmap<str, sqlite_bind_arg>);
-  done();
+  row(map::hashmap<str, sqlite_bind_arg>),
+  done(),
 }
 
 iface sqlite_stmt {
